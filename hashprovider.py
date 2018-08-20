8b855b2342f3804f68191058af04c86d84b818ad
ruiyangio <ruiyangwind@gmail.com>
2018-08-20 11:32:11 -0700

    Use uuid for item id

--- hashprovider.py
+++ hashprovider.py
@@ -1,8 +1,8 @@
-from hashlib import sha512
+from hashlib import sha256
 
-MAX_HASH_PLUS_ONE = 2**(sha512().digest_size * 8)
+MAX_HASH_PLUS_ONE = 2**(sha256().digest_size * 4)
 
 def get_hash_value(input_str):
     encoded = input_str.encode()
-    hash_digest = sha512(encoded).digest()
+    hash_digest = sha256(encoded).digest()
     return int.from_bytes(hash_digest, 'big')
--- test.py
+++ test.py
@@ -1,16 +1,23 @@
-from cluster import Cluster
+from cluster import Cluster, Item
+from consistent import ConsistentHash
+from uuid import uuid4
 
-test_cluster = Cluster(2)
+def build_test_cluster(n_nodes, n_item):
+    test_cluster = Cluster(n_nodes)
+    consistent = ConsistentHash("a", n_nodes)
+
+    for i in range(n_item):
+        item = Item(uuid4().hex, i)
+        col = consistent.get_column(str(item.id))
+        test_cluster.nodes[col].add_item(item)
+    
+    return test_cluster, consistent
+
+test_cluster, consistent = build_test_cluster(5, 500)
+
+print(len(test_cluster.nodes))
 
 for node in test_cluster.nodes:
-    print(node.id)
+    print(len(node.items))
 
-test_cluster.add_node()
-
-for node in test_cluster.nodes:
-    print(node.id)
-
-test_cluster.add_node()
-
-for node in test_cluster.nodes:
-    print(node.id)
+print(len(consistent.circle.keys()))
