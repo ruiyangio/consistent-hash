ea4cd0c83ab9440d5df84476a2cba099b75ead65
ruiyangio <ruiyangwind@gmail.com>
2018-08-30 14:12:16 -0700

    Version 1 done

--- cluster.py
+++ cluster.py
@@ -47,6 +47,7 @@ class Node(object):
                 continue
             for item_id in self.v_bucket_map[v_bucket_key]:
                 removed_items.append(self.items.pop(item_id))
+            self.v_bucket_map.pop(v_bucket_key)
         return removed_items
 
 class Cluster(object):
@@ -78,22 +79,26 @@ class Cluster(object):
     
     def get_item(self, item_id):
         column_id, v_bucket_key = self.meta.get_column_with_hash_key(self._make_hash_key(item_id))
-        return self.nodes[column_id].get_item(item_id)
+        return (self.nodes[column_id].get_item(item_id), column_id)
     
     def insert_item(self, item):
         column_id, v_bucket_key = self.meta.get_column_with_hash_key(item.hash_key)
         item.v_bucket_key = v_bucket_key
         self.nodes[column_id].add_item(item)
 
-    def generate_items(self, n):
+    def remove_item(self, item_id):
+        item, node_id = self.get_item(item_id)
+        self.nodes[node_id].remove_item(item.id)
+
+    def generate_items(self, n, do_report=True):
         for i in range(n):
             item_id = uuid.uuid4().hex
             item = Item(item_id, str(i), self._make_hash_key(item_id))
             self.insert_item(item)
         
-        self.report_uniformality()
+        return self.report_uniformality(do_report)
 
-    def add_node_and_rebalance(self):
+    def add_node_and_rebalance(self, do_report=True):
         self.n += 1
         new_column_id, v_bucket_to_move = self.meta.add_column()
         self.nodes[new_column_id] = Node(new_column_id)
@@ -103,9 +108,9 @@ class Cluster(object):
             for item in removed_items:
                 self.nodes[new_column_id].add_item(item)
         # Report uniformality
-        self.report_uniformality()
+        return self.report_uniformality(do_report)
     
-    def remove_node_and_rebalance(self, node_id):
+    def remove_node_and_rebalance(self, node_id, do_report=True):
         self.n -= 1
         v_bucket_to_move = self.meta.remove_column(node_id)
 
@@ -117,21 +122,25 @@ class Cluster(object):
 
         # Remove node
         self.nodes.pop(node_id)
-        self.report_uniformality()
+        return self.report_uniformality(do_report)
 
-    def report_uniformality(self):
+    def report_uniformality(self, do_report=True):
+        if not do_report:
+            return
+        is_uniform = False
         item_dist = self.get_item_dist()
         coeffient_of_variation = stats.variation(item_dist)
         total_count = self.get_item_count()
+        
+        print("-------------------------------------")
+        print("total: " + str(total_count))
+        print("coeffient_of_variation: " + str(coeffient_of_variation))
+        print(item_dist)
+        
         if util.test_uniformality(item_dist, len(self.nodes), total_count):
-            print("-------------------------------------")
-            print("total: " + str(total_count))
-            print("coeffient_of_variation: " + str(coeffient_of_variation))
-            print(item_dist)
+            is_uniform = True
             print("Items distribution is uniform")
         else:
-            print("-------------------------------------")
-            print("total: " + str(total_count))
-            print("coeffient_of_variation: " + str(coeffient_of_variation))
-            print(item_dist)
             print("Not uniform")
+        
+        return (is_uniform, coeffient_of_variation)
--- simulation.py
+++ simulation.py
@@ -10,39 +10,81 @@ def make_cluster(n_node, n_items, v_buckets):
     test_cluster.generate_items(n_items)
     return test_cluster
 
-def simulation(steps, n_node, n_items, v_buckets):
-    print("Create cluster: ")
+def print_seperator():
+    print("-------------------------------------")
+
+def scale_out(test_cluster, n_items, depth):
+    for i in range(depth):
+        test_cluster.generate_items(n_items, do_report=False)
+        test_cluster.add_node_and_rebalance(do_report=False)
+    test_cluster.report_uniformality()
+
+def process_report(report, coefficients):
+    uniform_count = 0
+    if report:
+        if report[0]:
+            uniform_count = 1
+        coefficients.append(report[1])
+    return uniform_count
+
+def simulation(steps, n_node, n_items, v_buckets, scale_out_times=5):
+    print("Creating cluster...")
     test_cluster = make_cluster(n_node, n_items, v_buckets)
-    operations = ["add_node", "delete_node", "add_items", "get_items"]
+    print("Cluster is created")
+    operations = ["add_node", "delete_node", "add_items", "get_items", "remove_items"]
+
+    print("Scale out {:d} times".format(scale_out_times))
+    scale_out(test_cluster, n_items, scale_out_times)
+    print_seperator()
+
+    uniform_count = 0
+    coefficients = []
 
     for i in range(steps):
         rand_operation = random.choice(operations)
 
         if rand_operation == "add_node":
-            print("-------------------------------------")
+            print_seperator()
             print("Add one node to cluster")
-            test_cluster.add_node_and_rebalance()
+            report = test_cluster.add_node_and_rebalance()
+            uniform_count += process_report(report, coefficients)
         elif rand_operation == "delete_node":
             node_ids = list(test_cluster.nodes.keys())
             # only remove if there is node
             if len(node_ids) > 1:
                 rand_node_id = random.choice(node_ids)
-                print("-------------------------------------")
+                print_seperator()
                 print("Remove node: " + str(rand_node_id))
-                test_cluster.remove_node_and_rebalance(rand_node_id)
+                report = test_cluster.remove_node_and_rebalance(rand_node_id)
+                uniform_count += process_report(report, coefficients)
         elif rand_operation == "add_items":
             n_items = random.randint(1000, 20000)
-            print("-------------------------------------")
+            print_seperator()
             print("Add items: " + str(n_items))
-            test_cluster.generate_items(n_items)
+            report = test_cluster.generate_items(n_items)
+            uniform_count += process_report(report, coefficients)
         elif rand_operation == "get_items":
             ids = test_cluster.get_all_item_ids()
             rand_ids = random.sample(ids, int(len(ids) * 0.3))
-            print("-------------------------------------")
+            print_seperator()
             print("Try get some amount of items: " + str(len(rand_ids)))
             for item_id in rand_ids:
-                item = test_cluster.get_item(item_id)
+                item, node_id = test_cluster.get_item(item_id)
                 if item.id != item_id:
                     raise ValueError('Expect: {} but got {}'.format(item_id, item.id))
+            uniform_count += 1
+        elif rand_operation == "remove_items":
+            ids = test_cluster.get_all_item_ids()
+            rand_ids = random.sample(ids, int(len(ids) * 0.15))
+            print_seperator()
+            print("Remove items: " + str(len(rand_ids)))
+            for item_id in rand_ids:
+                test_cluster.remove_item(item_id)
+            report = test_cluster.report_uniformality()
+            uniform_count += process_report(report, coefficients)
 
-simulation(10, 5, 100000, 10000)
+    print_seperator()
+    print("Total number of operations: {} Total number of operations result in even distribution: {:d}".format(steps, uniform_count))
+    print("Max coefficient of variation: {:f}".format(max(coefficients)))
+
+simulation(10, 5, 100000, 100000)
